c970049412ce011d5036ac44b310b87ced981168
chiranjit2020 <chiranjit.rick002@gmail.com>
2020-12-24 12:02:20 +0530

    first commit...

---
+++ upor.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const UPOR = (el,duration) => {
+
+	 let scrollToTop = function() {
+
+	  var scrollStep = -window.scrollY / (scrollDuration / 10);
+
+	  var scrollInterval = setInterval(function(){
+	    if ( window.scrollY != 0 ) {
+	        window.scrollBy( 0, scrollStep );
+	    }
+	    else clearInterval(scrollInterval); 
+	 },10);
+	}
+
+	let scrollDuration = duration;
+	el.addEventListener("click", scrollToTop.bind(duration));
+
+	window.addEventListener('scroll', () => {
+		if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+			const STYLE = {
+				opacity : '1',
+				transform : 'translateY(0)',
+				visibility : 'visible'
+			}
+			Object.assign(el.style, STYLE);
+		}else{
+			const STYLE = {
+				opacity : '0',
+				transform : 'translateY(100%)',
+				visibility : 'hidden'
+			}
+			Object.assign(el.style, STYLE);
+		}
+	})
+}
